e75c4520c71426ab56951da5ab9129a5d41fa691
Lucien Emmanuel Diop <67975600+manoudiopson@users.noreply.github.com>
2022-05-25 02:04:12 +0100

    add proxy-server with dynamic routing

---
+++ proxy-server/src/main/java/diop/lucien/proxyserver/ProxyServerApplication.java
@@ -0,0 +1,34 @@
+package diop.lucien.proxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServerApplication.class, args);
+    }
+
+    //    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r -> r.path("/product/**").uri("lb://PRODUCT-SERVICE"))
+                .build();
+
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator
+            (ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties)  {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+    }
+}
