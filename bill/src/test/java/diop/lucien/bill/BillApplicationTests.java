577396fb89d25b2a60f875b07e25473bf1eb6308
Lucien Emmanuel Diop <67975600+manoudiopson@users.noreply.github.com>
2022-05-25 02:32:28 +0100

    add product service

---
+++ bill/src/test/java/diop/lucien/bill/BillApplicationTests.java
@@ -0,0 +1,13 @@
+package diop.lucien.bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
