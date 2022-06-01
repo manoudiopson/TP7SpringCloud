2baf678121c9118a5253a369702596ddf0964c8d
Lucien Emmanuel Diop <67975600+manoudiopson@users.noreply.github.com>
2022-06-01 04:34:20 +0100

    add getBillDetails action is service & controller

---
+++ bill-service/src/main/java/diop/lucien/billservice/BillServiceApplication.java
@@ -0,0 +1,15 @@
+package diop.lucien.billservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class BillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillServiceApplication.class, args);
+    }
+
+}
